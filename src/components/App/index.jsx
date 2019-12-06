acd81a701fb4916c507a452ada276c4dd29b74a3
Serhii_Y <dj.ser961@gmail.com>
2019-12-06 04:21:42 +0200

    last

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Reader from '../Reader';
 import publications from '../publications.json'
 
 const App = () => (
-  <BrowserRouter>
+  <Router basename='/'>
     <Switch>
       <Route path='/reader' render={props => <Reader {...props} items={publications} />} />
       <Redirect to='/reader' />
     </Switch>
-  </BrowserRouter>
+  </Router>
 )
   
 export default App;
